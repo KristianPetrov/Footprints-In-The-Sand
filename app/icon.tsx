17cd336a3033cb9cdb74c491977d9dc7bd522c30
Kristian petrov <kristianpetrov@Kristians-Laptop.local>
2025-12-16 14:37:48 -0800

    added icon.tsx to generate favicon.ico

---
+++ app/icon.tsx
@@ -0,0 +1,45 @@
+import { ImageResponse } from 'next/og'
+import { readFile } from 'fs/promises'
+import { join } from 'path'
+
+export const size = {
+  width: 32,
+  height: 32,
+}
+export const contentType = 'image/png'
+
+export default async function Icon() {
+  const logoData = await readFile(
+    join(process.cwd(), 'public', 'footprints-in-the-sand-minimal-logo.png')
+  )
+  const logoBase64 = `data:image/png;base64,${logoData.toString('base64')}`
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          background: 'transparent',
+        }}
+      >
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src={logoBase64}
+          alt=""
+          width={32}
+          height={32}
+          style={{
+            objectFit: 'contain',
+          }}
+        />
+      </div>
+    ),
+    {
+      ...size,
+    }
+  )
+}
